35b6521ffa2b7b81cb34b83e0e5b12407943ee93
Josh Thomas <jthoms1@gmail.com>
2018-03-10 22:31:17 -0600

    updated more.

---
+++ packages/stencil-demo-components/www/build/stencil-demo-components/github-intro.js
@@ -0,0 +1,48 @@
+/*! Built with http://stenciljs.com */
+const { h, Context } = window.StencilDemoComponents;
+
+/**
+ * This component accepts a github userid and then displays a brief summary of their details
+ * including their avatar img.
+ */
+class GithubIntro {
+    constructor() {
+        /**
+         * The github userid for the user that you would like to display
+         */
+        this.userid = '';
+    }
+    fetchGithubData(userid) {
+        fetch(`https://api.github.com/users/${userid}`)
+            .then(rsp => rsp.json())
+            .then((data) => {
+            this.userData = data;
+        });
+    }
+    componentWillLoad() {
+        this.fetchGithubData(this.userid);
+    }
+    render() {
+        if (!this.userData) {
+            return null;
+        }
+        return [
+            h("small", null,
+                this.userData.name ?
+                    `${this.userData.name} / ` :
+                    null,
+                h("a", { href: this.userData.html_url },
+                    "@",
+                    this.userData.login)),
+            (this.userData.avatar_url) ?
+                h("img", { class: "roundup", src: this.userData.avatar_url, alt: "gravatar" }) :
+                null
+        ];
+    }
+    static get is() { return "github-intro"; }
+    static get encapsulation() { return "scoped"; }
+    static get properties() { return { "userData": { "state": true }, "userid": { "type": String, "attr": "userid", "watchCallbacks": ["fetchGithubData"] } }; }
+    static get style() { return "img.roundup {\n      border: 2px solid #fff;\n      height: 40px;\n      width: 40px;\n      border-radius: 23px;\n      display: block;\n      margin: -15px auto 0;\n    }"; }
+}
+
+export { GithubIntro };
---
+++ packages/stencil-demo-components/www/build/stencil-demo-components/img-mask.sc.js
@@ -0,0 +1,54 @@
+/*! Built with http://stenciljs.com */
+const { h, Context } = window.StencilDemoComponents;
+
+/**
+ * This component accepts an image and shows a window of view over a blur on the rest of the image
+ */
+class ImgMask {
+    constructor() {
+        /**
+         * The url to the image
+         */
+        this.src = '';
+        /**
+         * The width of the image
+         */
+        this.width = 0;
+        /**
+         * The height of the mask window
+         */
+        this.maskHeight = 0;
+        /**
+         * The starting location of the mask window
+         */
+        this.maskTop = 0;
+    }
+    hostData() {
+        return {
+            style: {
+                position: "relative",
+                overflow: "hidden"
+            }
+        };
+    }
+    render() {
+        return [
+            h("img", { width: this.width, class: "nostyle blur-filter", src: this.src }),
+            h("div", { class: "blur-window", style: {
+                    "width": `${this.width}px`,
+                    "background-size": `${this.width}px`,
+                    "background-image": `url(${this.src})`,
+                    "margin-right": `-${this.width / 2}px`,
+                    "top": `${this.maskTop}px`,
+                    "background-position": `0 -${this.maskTop}px`,
+                    "height": `${this.maskHeight}px`
+                } })
+        ];
+    }
+    static get is() { return "img-mask"; }
+    static get encapsulation() { return "scoped"; }
+    static get properties() { return { "maskHeight": { "type": Number, "attr": "mask-height" }, "maskTop": { "type": Number, "attr": "mask-top" }, "src": { "type": String, "attr": "src" }, "width": { "type": Number, "attr": "width" } }; }
+    static get style() { return ".blur-filter[data-img-mask] {\n      filter: blur(2px) grayscale(80%);\n    }\n    .blur-window[data-img-mask] {\n      position: absolute;\n      right: 50%;\n      box-shadow: 0 0 22px -2px rgba(0, 0, 0, 0.2)\n    }"; }
+}
+
+export { ImgMask };
