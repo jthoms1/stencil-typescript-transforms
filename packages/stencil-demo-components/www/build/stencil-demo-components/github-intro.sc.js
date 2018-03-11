ae2334c03c40467c0d031f8bd0a99dc5a684f163
Josh Thomas <jthoms1@gmail.com>
2018-03-10 22:35:59 -0600

    yep

--- packages/stencil-demo-components/www/build/stencil-demo-components/github-intro.sc.js
+++ packages/stencil-demo-components/www/build/stencil-demo-components/github-intro.sc.js
@@ -1,6 +1,7 @@
 /*! Built with http://stenciljs.com */
 const { h, Context } = window.StencilDemoComponents;
 
+<<<<<<< HEAD
 /**
  * This component accepts a github userid and then displays a brief summary of their details
  * including their avatar img.
@@ -10,6 +11,10 @@ class GithubIntro {
         /**
          * The github userid for the user that you would like to display
          */
+=======
+class GithubIntro {
+    constructor() {
+>>>>>>> yep
         this.userid = '';
     }
     fetchGithubData(userid) {
